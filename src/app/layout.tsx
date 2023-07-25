9151305cae4d7a47f924c230379f16bac6e444f4
Sinan Çimen <cimensinan89@gmail.com>
2023-07-25 23:33:54 +0300

    pizza resturant favicon and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,30 @@
-import Notification from '@/components/Notification'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import Notification from "@/components/Notification";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Massimo',
-  description: 'Magic of Taste, Hidden in Our Pizzas!',
-}
+  title: "Massimo",
+  description: "Magic of Taste, Hidden in Our Pizzas!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Notification/>
-        <Navbar/>
+        <Notification />
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
