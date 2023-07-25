b271d162cc2ebffe7448e66e4262703d331823ad
Sinan Çimen <cimensinan89@gmail.com>
2023-07-25 22:49:11 +0300

    pizza resturant category page

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,13 +1,16 @@
-"use client"
-import React from 'react'
-import Countdown from 'react-countdown'
+"use client";
+import React from "react";
+import Countdown from "react-countdown";
 
-const endingDate = new Date("2023-08-03")
+const endingDate = new Date("2023-08-03");
 
 const CountDown = () => {
   return (
-    <Countdown className='font-bold text-5xl text-yellow-300' date={endingDate} />
-  )
-}
+    <Countdown
+      className="font-bold text-5xl text-yellow-300"
+      date={endingDate}
+    />
+  );
+};
 
-export default CountDown
+export default CountDown;
